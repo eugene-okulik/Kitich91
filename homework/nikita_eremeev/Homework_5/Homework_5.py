d9d005ab43fd331ebec9e6c57aee50787deec2c4
kitich91 <kitich91@yandex.ru>
2026-05-27 18:16:29 +0300

    Fix  Homework_5

--- homework/nikita_eremeev/Homework_5/Homework_5.py
+++ homework/nikita_eremeev/Homework_5/Homework_5.py
@@ -1,20 +1,20 @@
 person = ['John', 'Doe', 'New York', '+1372829383739', 'US']
-name, last_name, city, phone, country =person
+name, last_name, city, phone, country = person
 print(name, last_name, city, phone, country)
 
 # Первая строка
 result1 = "результат операции: 42"
-num1 = int(result1[result1.index(':')+1:])
+num1 = int(result1[result1.index(':') + 1:].strip())
 print(num1 + 10)
 
 # Вторая строка
 result2 = "результат операции: 514"
-num2 = int(result2[result2.index(':')+1:])
+num2 = int(result2[result2.index(':') + 1:].strip())
 print(num2 + 10)
 
 # Третья строка
 result3 = "результат работы программы: 9"
-num3 = int(result3[result3.index(':')+1:])
+num3 = int(result3[result3.index(':') + 1:].strip())
 print(num3 + 10)
 
 students = ['Ivanov', 'Petrov', 'Sidorov']
