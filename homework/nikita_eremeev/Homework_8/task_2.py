322e07b14c37c500912ef9238ae419756024e7c9
kitich91 <kitich91@yandex.ru>
2026-06-15 18:21:04 +0300

    Fix flake8: add blank lines before function definition

--- homework/nikita_eremeev/Homework_8/task_2.py
+++ homework/nikita_eremeev/Homework_8/task_2.py
@@ -1,17 +1,15 @@
 import sys
 
-# Увеличиваем лимит для конвертации больших чисел в строку (0 означает без лимита)
 sys.set_int_max_str_digits(100000)
 
+
 def fibonacci_generator():
-    """Бесконечный генератор чисел Фибоначчи"""
     a, b = 0, 1
     while True:
         yield a
         a, b = b, a + b
 
 
-# Получаем нужные числа
 for index, number in enumerate(fibonacci_generator(), start=1):
     if index == 5:
         print(f"5-е число: {number}")
